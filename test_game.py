c103e47ea6b1379b00c12f4cc14e10610c38dff9
Georgia Lynott <georgialynott@gmail.com>
2021-06-04 15:15:31 +0100

    Adds discard to crib and starter card to `test_game`

--- test_game.py
+++ test_game.py
@@ -16,4 +16,12 @@ print(player_2)
 
 # Each player discards two cards to the crib
 crib = []
-crib.append(player_1.discard([1, 2]))
+crib.extend(player_1.discard([1, 2]))
+crib.extend(player_2.discard([0, 3]))
+print(crib)
+print(player_1)
+print(player_2)
+
+# Turn over starter card
+starter_card = deck.draw(1)
+print(starter_card)
