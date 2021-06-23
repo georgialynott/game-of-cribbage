f9a86547efa52268e04c3eea749eb80acec1a933
Georgia Lynott <georgialynott@gmail.com>
2021-06-23 19:37:26 +0100

    Add `while` loop for gameplay, now assigns a dealer for each round

--- test_game.py
+++ test_game.py
@@ -7,27 +7,66 @@ from game_mechanics import Card, Deck, Player
 deck = Deck()
 deck.shuffle()
 
-# Create players and deal each a hand of 6
-player_1 = Player(input('Player 1 enter name: '), deck.draw(6))
-player_2 = Player(input('Player 2 enter name: '), deck.draw(6))
-
+# Create players with user-input names
+player_1 = Player(input('Player 1 enter name: '))
+player_2 = Player(input('Player 2 enter name: '))
 print(player_1)
 print(player_2)
 
-# Each player discards two cards to the crib
-crib = []
-print(player_1.name)
-player_1_discard = [int(input('Choose 1st card to discard to crib: ')),
-                    int(input('Choose 2nd card to discard to crib: '))]
-print(player_2.name)
-player_2_discard = [int(input('Choose 1st card to discard to crib: ')),
-                    int(input('Choose 2nd card to discard to crib: '))]
-crib.extend(player_1.discard(player_1_discard))
-crib.extend(player_2.discard(player_2_discard))
-print('Crib: ', crib)
-print(player_1.name, player_1.hand)
-print(player_2.name, player_2.hand)
+# Create counter for rounds
+round_number = 0
 
-# Turn over starter card
-starter_card = deck.draw(1)
-print('Starter ', starter_card)
+# Begin loop over rounds until either player reaches score of 121
+while player_1.score & player_2.score < 121:
+
+    # Start new round and print current scores
+    round_number += 1
+    print('Round {r}\n{p1}: {s1}, {p2}: {s2}'.format(r = round_number, 
+                                                       p1 = player_1.name, 
+                                                       s1 = player_1.score, 
+                                                       p2 = player_2.name, 
+                                                       s2 = player_2.score))
+
+    # Select dealer based on round
+    dealer = [player_1, player_2][round_number%2]
+
+    # Deal each player a hand of 6
+    player_1.hand = deck.draw(6)
+    player_2.hand = deck.draw(6)
+    print(player_1.name, player_1.hand)
+    print(player_2.name, player_2.hand)
+
+    # Each player discards two cards to the crib
+    crib = []
+    # Get discarded cards from players
+    print(player_1.name)
+    player_1_discard = [int(input('Choose 1st card to discard to crib: '))-1,
+                    int(input('Choose 2nd card to discard to crib: '))-1]
+    print(player_2.name)
+    player_2_discard = [int(input('Choose 1st card to discard to crib: '))-1,
+                    int(input('Choose 2nd card to discard to crib: '))-1]
+    # Add discarded cards to crib
+    crib.extend(player_1.discard(player_1_discard))
+    crib.extend(player_2.discard(player_2_discard))
+    print('Crib: ', crib)
+    # Print updated player hands
+    print(player_1.name, player_1.hand)
+    print(player_2.name, player_2.hand)
+
+    # Turn over starter card
+    starter_card = deck.draw(1)[0]
+    print('Starter ', starter_card)
+    # Check if starter card is a Jack
+    if starter_card.rank == 'J':
+        dealer.peg(2) # dealer pegs 2 points for "his heels"
+    
+    # THE PLAY
+    # --------
+    # Starting with the dealers, players take turns placing cards
+    # up to a cumlative value of 31. If a player cannot play a card
+    # without going over 31, they say "Go". The other player must then
+    # place any remaining cards they can from their hand, without
+    # exceeding 31. Play then restarts with the player who called 
+    # "Go" playing first.
+
+    
